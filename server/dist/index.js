2973436aa045b0cf8366ab14ba3435b6e9102b00
wasif kareem <wasifkareem2762@gmail.com>
2023-10-31 21:40:46 +0530

    cloudinary added

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -5,34 +14,57 @@ import cors from "cors";
 import courseRoute, { addCourse } from "./routes/courses.js";
 import authRoute from "./routes/auth.js";
 import helmet from "helmet";
-import bodyParser from "body-parser";
-import multer from "multer";
+import { fileURLToPath } from "url";
+import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+import Multer from "multer";
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cors({ origin: "*", methods: "GET,PUT,POST,DELETE" }));
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: "30mb" }));
 mongoose
     .connect(String(process.env.MONGO_URL))
     .then(() => console.log("DB is online"))
     .catch((err) => {
     console.log(err);
 });
-app.use("/assets", express.static("../public/assets"));
-//File storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    },
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
-const upload = multer({ storage });
-app.post("/courses/addCourse", upload.fields([{ name: "videoFile" }, { name: "imgFile" }]), addCourse);
+function handleUpload(file) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield cloudinary.uploader.upload(file, {
+            resource_type: "auto",
+        });
+        return res;
+    });
+}
+const storage = Multer.memoryStorage();
+const upload = Multer({
+    storage,
+});
+app.post("/upload", upload.single("my_file"), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const b64 = Buffer.from(req.file.buffer).toString("base64");
+        let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+        const cldRes = yield handleUpload(dataURI);
+        res.json(cldRes);
+    }
+    catch (error) {
+        console.log(error);
+        res.send({
+            message: error.message,
+        });
+    }
+}));
+app.post("/courses/addCourse", upload.fields([]), addCourse);
 app.use("/courses", courseRoute);
 app.use("/auth", authRoute);
 app.listen(port, () => {
--- server/index.ts
+++ server/index.ts
@@ -5,10 +5,10 @@ import cors from "cors";
 import courseRoute, { addCourse } from "./routes/courses.js";
 import authRoute from "./routes/auth.js";
 import helmet from "helmet";
-import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import path from "path";
-import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import Multer from "multer";
 
 const app = express();
 const port = 3000;
@@ -18,9 +18,8 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(cors({ origin: "*", methods: "GET,PUT,POST,DELETE" }));
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json({ limit: "30mb" }));
 
 mongoose
   .connect(String(process.env.MONGO_URL))
@@ -29,25 +28,38 @@ mongoose
     console.log(err);
   });
 
-app.use("/assets", express.static(path.join(__dirname, "../public/assets")));
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+async function handleUpload(file) {
+  const res = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return res;
+}
 
-//File storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+const storage = Multer.memoryStorage();
+const upload = Multer({
+  storage,
 });
 
-const upload = multer({ storage });
+app.post("/upload", upload.single("my_file"), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    const cldRes = await handleUpload(dataURI);
+    res.json(cldRes);
+  } catch (error) {
+    console.log(error);
+    res.send({
+      message: error.message,
+    });
+  }
+});
 
-app.post(
-  "/courses/addCourse",
-  upload.fields([{ name: "videoFile" }, { name: "imgFile" }]),
-  addCourse
-);
+app.post("/courses/addCourse", upload.fields([]), addCourse);
 app.use("/courses", courseRoute);
 app.use("/auth", authRoute);
 
