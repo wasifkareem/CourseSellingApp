d5cc24edca9546a7088f4b1cb6e628488eecc834
wasif kareem <wasifkareem2762@gmail.com>
2023-11-01 11:44:34 +0530

    video shadow added

--- clientts/tailwind.config.ts
+++ clientts/tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
           "url('https://motionarray.imgix.net/preview-1758502-2of8u0oaQveiflLB-large.jpg?w=660&q=60&fit=max&auto=format')",
         "footer-texture": "url('/img/footer-texture.png')",
       },
+      boxShadow: {
+        "custom-blue": "0 4px 25px -4px #006bb7, 0 0px 20px -6px #006bb7",
+      },
     },
   },
   plugins: [],
